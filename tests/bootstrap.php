e3a9a85d26dea2a76a6c73d09ad5d986446817a0
Florian Meyer <florian.meyer@project-engineers.de>
2023-02-21 17:19:09 +0100

    updated doctrine

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\CachedReader;
-use Doctrine\Common\Cache\ArrayCache;
 use Composer\Autoload\ClassLoader;
 define('TESTS_PATH', __DIR__);
 define('TESTS_TEMP_DIR', __DIR__ . '/temp');
@@ -10,10 +8,10 @@ define('VENDOR_PATH', realpath(__DIR__ . '/../vendor'));
 
 /** @var $loader ClassLoader */
 $loader = require __DIR__ . '/../vendor/autoload.php';
-AnnotationRegistry::registerLoader(array(
-	$loader,
-	'loadClass' 
-));
+//AnnotationRegistry::registerLoader(array(
+//	$loader,
+//	'loadClass'
+//));
 //Gedmo\DoctrineExtensions::registerAnnotations();
 
 $reader = new AnnotationReader();
